076f0752fb5296132d535ada06eaa22950fe700c
Helen Hood <helen.c.hood@gmail.com>
2014-07-01 13:12:08 -0400

    Simplify add new car spec

--- spec/features/add_new_car_spec.rb
+++ spec/features/add_new_car_spec.rb
@@ -13,16 +13,16 @@ feature 'add a new car to the lot', %q(
   # If I do not specify all of the required information in the required formats,
     # the car is not recorded and I am presented with errors.
   # Upon successfully creating a car, I am redirected back to the index of cars.
-    
+
   scenario 'new car submitted with valid information' do
     man = FactoryGirl.create(:manufacturer)
 
     visit '/cars/new'
 
     within '#car_manufacturer_id' do
-      find(:xpath, "option[2]").select_option
+      select man.name
     end
-    
+
     fill_in 'Color', with: 'Beige'
     fill_in 'Year', with: '1993'
     fill_in 'Mileage', with: '111234'
@@ -32,7 +32,7 @@ feature 'add a new car to the lot', %q(
     expect(page).to have_content man.name
   end
 
-  scenario 'new car submitted with invalid information' do 
+  scenario 'new car submitted with invalid information' do
     FactoryGirl.create(:manufacturer)
 
     visit '/cars/new'
@@ -40,7 +40,7 @@ feature 'add a new car to the lot', %q(
     fill_in 'Color', with: '2006'
     fill_in 'Mileage', with: 'Boo'
     click_button 'Add Car'
-    expect(page).to have_content 'Invalid submission. Please include all 
+    expect(page).to have_content 'Invalid submission. Please include all
       required information about the car'
   end
 end
