17ef7b0d5213b2aa7fef0e977f9b04b2a0b85996
Helen Hood <helen.c.hood@gmail.com>
2014-07-01 13:13:09 -0400

    Fix whitespace and indentation

--- spec/features/add_new_manufacturer_spec.rb
+++ spec/features/add_new_manufacturer_spec.rb
@@ -8,27 +8,28 @@ feature "add new manufacturer to list", %q(
   # I must specify a manufacturer name and country.
   # If I do not specify the required information,
   #   I am presented with errors.
-  # If I specify the required information, 
-  #   the manufacturer is recorded and 
+  # If I specify the required information,
+  #   the manufacturer is recorded and
   #   I am redirected to the index of manufacturers
-  
+
   scenario "user adds new manufacturer with valid information" do
     visit '/manufacturers/new'
     fill_in 'Name', with: 'Alfa Romeo'
     fill_in 'Country', with: 'Italy'
     click_button 'Add Manufacturer'
+
     expect(page).to have_content('Successfully added new manufacturer')
     expect(page).to have_content('Alfa Romeo')
     expect(page).to have_content('Italy')
   end
 
-  scenario "user adds manufacturer with invalid information" do 
+  scenario "user adds manufacturer with invalid information" do
     visit '/manufacturers/new'
     fill_in 'Name', with: 'Car Maker'
     click_button 'Add Manufacturer'
-    expect(page).to have_content('Invalid submission. Please include all 
-      required information about the manufacturer')
-  expect(page).to_not have_content 'Car Maker'
-  end
 
+    expect(page).to have_content('Invalid submission. Please include all
+      required information about the manufacturer')
+    expect(page).to_not have_content 'Car Maker'
+  end
 end
